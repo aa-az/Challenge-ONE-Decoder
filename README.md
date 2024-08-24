dbad9d5c418ec6f453fcfa41b4634605cdf34af3
aa-az <aa_az@hotmail.com>
2024-08-24 18:57:25 -0500

    Upload de los archivos del proyecto.
    Se subieron todos los archivos del proyecto.

---
+++ README.md
@@ -0,0 +1,52 @@
+﻿**CHALLENGE ONE: ENCRIPTADOR DE TEXTO.**
+
+Este proyecto es el primero de los **“Challenges”** propuestos por la Plataforma **ALURA LATAM – ORACLE ONE** a sus alumnos del curso de JavaScript, siguiendo la metodología de **“Aprender haciendo”**, lo cual permite al participante aplicar los conocimientos adquiridos y ganar experiencia enriquecedora.
+
+El proyecto propuesto consiste básicamente en **“encriptar”** un texto, cambiando las vocales por un texto determinado y devolver la frase resultante al usuario. De modo similar, también debe ofrecerse al usuario la operación de **“desencriptado”**, devolviendo el texto encriptado a su versión original. Como añadidura, debe darse al usuario la oportunidad de **“copiar”** la frase obtenida en el proceso de encriptación a través de una opción dispuesta para ello en la interface con el usuario.
+
+**Consideraciones del proyecto:**
+
+1\.- Disponer una interfaz sencilla que permita al usuario una interacción fácil y rápida.
+
+2\.- La interfaz debe contener dos **“cajas de texto”**: En la primera se ingresará el texto a **“encriptar”** y en la segunda se recuperará el texto ya **“encriptado”**.
+
+3\.- Deberá permitirse que el usuario copie la frase o palabra obtenida en el proceso de encriptación.
+
+4\.- Deberá permitírsele al usuario **“desencriptar”** la palabra o frase de manera conveniente a través de las mismas cajas de texto usadas en el procesos de encriptado.
+
+5\.- La interfaz de usuario deberá tener los botones: **“encriptar”**, **“desencriptar”** y **“copiar”**, convenientemente dispuestos y de fácil acceso.
+
+6\.- El usuario tendrá una limitación en el ingreso de textos, la cual pasa por solo permitírsele el ingreso de **“letras minúsculas, sin acentos y sin caracteres especiales”**.
+
+7\.- Para el proceso de encriptado deberá tenerse en consideración las siguientes equivalencias:
+
+La letra **"e"** será igual al texto:**"enter"**
+
+La letra **"i"** será igual al texto:**"imes"**
+
+La letra **"a"** será igual al texto:**"ai"**
+
+La letra **"o"** será igual al texto:**"ober"**
+
+La letra **"u"** será igual al texto:**"ufat"**
+
+**Resolución del proyecto:**
+
+El proyecto fue resuelto utilizando **HTML, CSS Y JAVASCRIPT**, así como las herramientas **FIGMA, TRELLO y VISUAL STUDIO CODE**.
+
+**Agradecimientos:**
+
+` `Mi más profundo agradecimiento a la Plataforma Educativa **ALURA LATAM** que en convenio con **ORACLE ONE** están brindando una capacitación de calidad, permitiendo a los alumnos formarse con los más altos estándares educativos y solventando así las exigencias del mercado.
+
+Además quiero agradecer a los instructores: **Bárbara Santos, Jeanmarie Quijada, Ellen Pimentel, Génesys Rondón, Christian Velasco, Leonardo José Castillo Lacruz, Gabriela Aguiar y Priscila Stuani**; quienes con sus instrucciones y charlas enfocadas en el desarrollo de Javascript y en el desarrollo personal, permitieron que mi aprendizaje sea más profundo y personal, enriqueciendo mis conocimientos y mi espíritu.
+
+Gracias amigos profesores, nos vemos en el camino.
+
+**Notas:**
+
+El desarrollo del proyecto propuesto es de mi autoría, si bien no es perfecto, estaré gustoso de escuchar sugerencias, pueden contactarme en el email: <aa_az@hotmail.com>
+
+Las gráficas mostradas como fondos de la aplicación son de mi autoría, fueron realizadas a través de la **creación y diseño de prompt específico** para luego ser aplicado en **DALL-E3**. 
+
+**©2024 Arturo Alejandro Aguirre Zevallos.**
+
---
+++ scripts.js
@@ -0,0 +1,164 @@
+let btnEncrypt = document.getElementById("button_encrypt");
+let btnDecrypt = document.getElementById("button_decrypt");
+let btnCopy = document.getElementById("button_copy");
+let textInput = document.getElementById("input_text_encrypt");
+let textOutput = document.getElementById("output_text_decrypt");
+let container = document.querySelector(".result");
+
+const ERRORCLIPBOARD = 'Error al copiar al portapapeles:'
+const ERRORCOPY = "Error al copiar el mensaje"
+const MSGCOPIED = "El mensaje ha sido copiado"
+const NONECOPY = "No hay nada que copiar"
+const NOTHINGTODECRYPT = "Ingrese un mensaje a ser desencriptado en el área de texto."
+const NOTHINGTOENCRYPT = "Por favor, ingrese el mensaje a ser encriptado en el área de texto."
+const REMINDER = "Sólo debe ingresar texto en minúsculas, sin acentos y sin caracteres especiales."
+
+const backgrounds = [
+      'url("img/background-a.jpg")',
+      'url("img/background-b.jpg")',
+      'url("img/background-c.jpg")'
+    ];
+
+const encryptDictionary = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat'
+};
+
+const decryptDictionary = {
+    'ai': 'a',
+    'enter': 'e',
+    'imes': 'i',
+    'ober': 'o',
+    'ufat': 'u'
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('input_text_encrypt');
+    const errorMessage = document.getElementById('errorMessage');
+    
+    textarea.addEventListener('input', () => {
+        const inputValue = textarea.value;
+        
+        // Normalizar el texto y convertirlo a minúsculas
+        const normalizedValue = inputValue.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+        const lowercaseValue = normalizedValue.toLowerCase();
+
+        // Filtrar caracteres válidos (solo letras y espacios)
+        const validValue = lowercaseValue.replace(/[^a-z\s]/g, '');
+
+        // Verificar si el valor actual es válido
+        if (inputValue !== validValue) {
+            // Si no es válido, solo mantiene los caracteres válidos
+            textarea.value = validValue;
+            errorMessage.textContent = REMINDER;
+        } else {
+            errorMessage.textContent = ''; 
+        }
+    });
+});
+
+function setRandomBackground() {
+  const randomIndex = Math.floor(Math.random() * backgrounds.length);
+  document.body.style.backgroundImage = backgrounds[randomIndex];
+}
+
+function encrypt(text) {
+    return text.split('').map(character => encryptDictionary[character] || character).join('');
+}
+
+function decrypt(text) {
+    // Reemplazar primero las secuencias más largas para evitar reemplazos parciales
+    let result = text;
+    Object.keys(decryptDictionary).forEach(key => {
+        const value = decryptDictionary[key];
+        const regex = new RegExp(key, 'g');
+        result = result.replace(regex, value);
+    });
+    return result;
+}
+
+function showEncrypted() {
+	if (textInput.value != "") {
+		let textEncrypted = encrypt(textInput.value);
+		textOutput.innerHTML = textEncrypted;
+		textOutput.value = textEncrypted;
+		Refresh();
+	} else {
+    myAlert(NOTHINGTOENCRYPT);
+    focusTextArea();
+	}		
+}		
+		
+function showDecrypted() {
+	if (textInput.value != "") {
+        let textDecrypted = decrypt(textInput.value);
+        textOutput.innerHTML = textDecrypted;
+        textOutput.value = textDecrypted;
+        Refresh();
+    }
+    else {
+        myAlert(NOTHINGTODECRYPT);
+        focusTextArea();
+    }
+}	
+
+function copyMessage() {
+    if (textOutput && textOutput.value.trim() !== "") {
+        navigator.clipboard.writeText(textOutput.value)
+            .then(() => {
+                myAlert(MSGCOPIED);
+            })
+            .catch(err => {
+                console.error(ERRORCLIPBOARD, err);
+                myAlert(ERRORCOPY);
+            });
+    } else {
+        myAlert(NONECOPY);
+    }
+}		
+		
+function myAlert(message) {
+	var overlay = document.getElementById('overlay');
+	var alert = document.getElementById('custom-alert');
+	
+	overlay.style.display = 'block';
+	alert.innerHTML = message;
+	alert.classList.remove('fade-out');
+	alert.classList.add('fade-in');
+	alert.style.display = 'block';
+
+	setTimeout(function() {
+		alert.classList.remove('fade-in');
+		alert.classList.add('fade-out');
+		setTimeout(function() {
+			alert.style.display = 'none';
+			overlay.style.display = 'none';
+		}, 500);
+	}, 2000);
+}		
+
+function enableButtons() {
+    btnEncrypt.disabled = false;
+    btnDecrypt.disabled = false;    
+}
+
+function Refresh() {
+    if(textInput.value !== ""){
+        container.classList.remove("no_texto")
+    }
+    textInput.focus();
+}
+
+function focusTextArea() {
+    var textarea = document.getElementById("input_text_encrypt");
+    textarea.focus();
+}
+
+window.onload = setRandomBackground;
+btnEncrypt.onclick = showEncrypted;
+btnDecrypt.onclick = showDecrypted;
+btnCopy.onclick = copyMessage;
+textInput.onclick = enableButtons;
